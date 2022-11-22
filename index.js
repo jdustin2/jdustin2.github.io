bde3caf0872f1657c6f573e66d2036ac1b3740f4
jdustin2 <jdustin@pdx.edu>
2022-11-22 11:21:30 -0800

    Add more info about me and dynamically load projects

---
+++ index.js
@@ -0,0 +1,18 @@
+const projects = document.querySelector("#projects");
+
+results = fetch("https://api.github.com/users/jdustin2/repos", {
+  headers: {
+    Accept: "application/vnd.github.v3+json",
+  },
+})
+  .then((body) => body.json())
+  .then((results) => {
+    results.forEach((repo) => {
+      projects.innerHTML +=
+        `<br/><div class="githubProject"><a href='${repo.html_url}' target=blank><h6>${repo.name}</h6></a>` +
+        repo.description +
+        "<br/>" +
+        new Date(repo.created_at).toDateString() +
+        "</div>";
+    });
+  });
